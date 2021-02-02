2354fb503d8f3e7c1ab52bbcc73b40c97b94029c
Kylie <ka-scott@hotmail.co.uk>
2021-02-02 15:11:30 +0000

    ex. 1.1 (refactor to split into 3 components

--- course_info/src/index.js
+++ course_info/src/index.js
@@ -13,19 +13,38 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <Header course = {course} />
+        <Content part= {part1} exercises= {exercises1} />
+        <Content part = {part2} exercises= {exercises2} />
+        <Content part = {part3} exercises = {exercises3} />
+        <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
+}
+
+// components used in App (after being sent data)
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <p>Section: {props.part}  No. of exercises: {props.exercises}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>Total no. of exercises: {props.total}</p>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
